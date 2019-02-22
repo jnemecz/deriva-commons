5edc732dcb42b30dfb07c83eb4a5b37e31bfbcc4
jnemecz <jiri.nemec@gmail.com>
2019-02-22 16:57:31 +0100

    Validace emailu

--- src/main/java/cz/deriva/commons/ValidationUtils.java
+++ src/main/java/cz/deriva/commons/ValidationUtils.java
@@ -34,8 +34,16 @@ public final class ValidationUtils {
   }
 
   public static boolean isValidEmail(final CharSequence email) {
-    // TODO: dodelat validaci!
-    return StringUtils.isNotBlank(email);
+
+    if(StringUtils.isBlank(email)){
+      return false;
+    }
+
+    final String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+
+    java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+    java.util.regex.Matcher m = p.matcher(email);
+    return m.matches();
   }
 
   /**
