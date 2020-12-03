046fe8c73c9399653d3d994cc2963b1cfac7c2f8
jnemecz <jiri.nemec@gmail.com>
2020-12-03 17:21:46 +0100

    Prevod Entity z abstract class na interface

--- src/main/java/cz/deriva/commons/data/domain/Entity.java
+++ src/main/java/cz/deriva/commons/data/domain/Entity.java
@@ -1,26 +1,9 @@
 package cz.deriva.commons.data.domain;
 
-import cz.deriva.commons.utils.AssertUtils;
-
-import java.time.LocalDateTime;
-
 /**
  * An entity, as explained in the DDD book.
  */
-public abstract class Entity<T extends Entity, ID extends RecordId> {
-
-  private final ID id;
-  private final LocalDateTime itime;
-
-  protected Entity(final ID id, final LocalDateTime itime) {
-
-    AssertUtils.notNull(id, "id");
-    this.id = id;
-
-    AssertUtils.notNull(itime, "itime");
-    this.itime = itime;
-
-  }
+public interface Entity<T, ID> {
 
   /**
    * Entities compare by identity, not by attributes.
@@ -28,22 +11,13 @@ public abstract class Entity<T extends Entity, ID extends RecordId> {
    * @param other The other entity.
    * @return true if the identities are the same, regardless of other attributes.
    */
-  public boolean sameIdentityAs(final T other) {
-    AssertUtils.notNull(other, "other");
-    return this.getId().sameAs(other.getId());
-  }
+  boolean sameIdentityAs(final T other);
 
   /**
    * Vraci ID dane entity.
    *
    * @return id entity
    */
-  public ID getId() {
-    return this.id;
-  }
-
-  public LocalDateTime getItime() {
-    return itime;
-  }
+  ID getId();
 
 }
