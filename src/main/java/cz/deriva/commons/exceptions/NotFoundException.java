9bd66e64b508f237ef938bbfa7e9f70a64c605a5
jnemecz <jiri.nemec@gmail.com>
2020-12-30 17:06:24 +0100

    1.1.27

---
+++ src/main/java/cz/deriva/commons/exceptions/NotFoundException.java
@@ -0,0 +1,27 @@
+package cz.deriva.commons.exceptions;
+
+import cz.deriva.commons.data.domain.Entity;
+import cz.deriva.commons.data.domain.RecordId;
+
+/**
+ * Identifikuje chybejici entitu (pouzivat prioritne oproti RecordNotFoundException)
+ */
+public class NotFoundException extends RuntimeException {
+
+  private final Class<? extends Entity> clazz;
+  private final RecordId id;
+
+  public NotFoundException(final Class<? extends Entity> clazz, final RecordId id) {
+    this.clazz = clazz;
+    this.id = id;
+  }
+
+  public Class<? extends Entity> getClazz() {
+    return clazz;
+  }
+
+  public RecordId getId() {
+    return id;
+  }
+
+}
