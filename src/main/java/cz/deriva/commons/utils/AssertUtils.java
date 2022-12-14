54f6e8baddae9a266507519717a8f5ea693678af
jnemecz <jiri.nemec@gmail.com>
2022-12-14 14:28:35 +0100

    Vyvoj

--- src/main/java/cz/deriva/commons/utils/AssertUtils.java
+++ src/main/java/cz/deriva/commons/utils/AssertUtils.java
@@ -9,183 +9,262 @@ import java.math.BigDecimal;
  */
 public final class AssertUtils {
 
-  private AssertUtils() {
-  }
-
-  public static String emailAddress(final String reference, final String parameterName) {
-    AssertUtils.notNull(reference, parameterName);
-    if (!ValidationUtils.isValidEmail(reference)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be valid e-mail address", parameterName));
-    }
-    return reference;
-  }
-
-  public static String notBlank(final String reference, final String parameterName) {
-    AssertUtils.notNull(reference, parameterName);
-    if (StringUtils.isBlank(reference)) {
-      raiseNotBlankError(parameterName);
-    }
-    return reference;
-  }
-
-  public static void raiseError(final String message) {
-    throw new IllegalArgumentException(message);
-  }
-
-  public static void raiseNullError(final String fieldName) {
-    if (StringUtils.isNotBlank(fieldName)) {
-      raiseError(String.format("[Assertion failed] - argument %s must be null", fieldName));
-    } else {
-      raiseError("[Assertion failed] - argument must be null");
-    }
-  }
-  public static void raiseNotNullError(final String fieldName) {
-    if (StringUtils.isNotBlank(fieldName)) {
-      raiseError(String.format("[Assertion failed] - argument %s is required; it must not be null", fieldName));
-    } else {
-      raiseError("[Assertion failed] - argument is required; it must not be null");
-    }
-  }
-
-  public static void raiseNotBlankError(final String fieldName) {
-    if (StringUtils.isNotBlank(fieldName)) {
-      raiseError(String.format("[Assertion failed] - argument %s is required; it must not be blank", fieldName));
-    } else {
-      raiseError("[Assertion failed] - argument is required; it must not be blank");
-    }
-  }
-
-  public static void isGteZero(final Double value, final String parameterName) {
-    if (!ValidationUtils.isGteZero(value)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0", parameterName));
-    }
-  }
-
-  public static void isGteZero(final Long value, final String parameterName) {
-    if (!ValidationUtils.isGteZero(value)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0", parameterName));
-    }
-  }
-
-  public static void isGteZero(final BigDecimal value, final String parameterName) {
-    if (!ValidationUtils.isGteZero(value)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0", parameterName));
-    }
-  }
-
-  public static void isGteOne(final BigDecimal value, final String parameterName) {
-    if (!ValidationUtils.isGte(value, BigDecimal.ONE)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 1", parameterName));
-    }
-  }
-
-  public static void isGte(final BigDecimal checkValue, final BigDecimal intervalValue, final String parameterName) {
-    if (!ValidationUtils.isGte(checkValue, intervalValue)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be greater or equal than %s", parameterName, intervalValue));
-    }
-  }
-
-  public static void isSame(final BigDecimal value1, final BigDecimal value2, final String parameterName) {
-    if (!ValidationUtils.isSame(value1, value2)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be same to %s", parameterName, value2));
-    }
-  }
-
-  public static void isLte(final BigDecimal checkValue, final BigDecimal intervalValue, final String parameterName) {
-    if (!ValidationUtils.isLte(checkValue, intervalValue)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be lower or equal than %s", parameterName, intervalValue));
-    }
-  }
-
-  public static void isGt(final BigDecimal checkValue, final BigDecimal intervalValue, final String parameterName) {
-    if (!ValidationUtils.isGt(checkValue, intervalValue)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be greater or equal than %s", parameterName, intervalValue));
-    }
-  }
-
-  public static void isLt(final BigDecimal checkValue, final BigDecimal intervalValue, final String parameterName) {
-    if (!ValidationUtils.isLt(checkValue, intervalValue)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be lower or equal than %s", parameterName, intervalValue));
-    }
-  }
-
-  public static void isLteOne(final BigDecimal value, final String parameterName) {
-    if (!ValidationUtils.isLte(value, BigDecimal.ONE)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be lower or equal than 1", parameterName));
-    }
-  }
-
-  public static void isGtZero(final Long value, final String parameterName) {
-    if (!ValidationUtils.isGtZero(value)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0", parameterName));
-    }
-  }
-
-  public static void between(final BigDecimal value, final BigDecimal fromInclusive, final BigDecimal toInclusive, final String parameterName) {
-
-    if (!ValidationUtils.between(value, fromInclusive, toInclusive)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is not within the range", parameterName));
+    private AssertUtils() {
     }
 
-  }
-
-  public static void isGtZero(final BigDecimal value, final String parameterName) {
-    if (!ValidationUtils.isGtZero(value)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be greater than 0", parameterName));
+    public static String emailAddress(final String reference, final String parameterName) {
+        AssertUtils.notNull(reference, parameterName);
+        if (!ValidationUtils.isValidEmail(reference)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be valid e-mail address",
+                            parameterName));
+        }
+        return reference;
     }
-  }
 
-  public static void isGtZero(final Double value, final String parameterName) {
-    if (!ValidationUtils.isGtZero(value)) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s is required; it must be greater than 0", parameterName));
+    public static String notBlank(final String reference, final String parameterName) {
+        AssertUtils.notNull(reference, parameterName);
+        if (StringUtils.isBlank(reference)) {
+            raiseNotBlankError(parameterName);
+        }
+        return reference;
     }
-  }
 
-  public static void validState(final boolean expression, final String message) {
-    if (!expression) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - the validated state is false; %s", message));
+    public static void raiseError(final String message) {
+        throw new IllegalArgumentException(message);
     }
-  }
 
-  public static <T> T forceNull(final T reference) {
-    if (reference != null) {
-      raiseNullError(null);
+    public static <T extends CharSequence> T raiseNullError(final T fieldName) {
+        if (StringUtils.isNotBlank(fieldName)) {
+            raiseError(String.format("[Assertion failed] - argument %s must be null", fieldName));
+        } else {
+            raiseError("[Assertion failed] - argument must be null");
+        }
+        return fieldName;
     }
-    return reference;
-  }
 
-  public static <T> T forceNull(final T reference, final String parameterName) {
-    if (reference != null) {
-      raiseNullError(parameterName);
+    public static void raiseNotNullError(final String fieldName) {
+        if (StringUtils.isNotBlank(fieldName)) {
+            raiseError(String.format("[Assertion failed] - argument %s is required; it must not be null", fieldName));
+        } else {
+            raiseError("[Assertion failed] - argument is required; it must not be null");
+        }
     }
-    return reference;
-  }
 
-  public static <T> T notNull(final T reference) {
-    if (reference == null) {
-      raiseNotNullError(null);
+    public static void raiseNotBlankError(final String fieldName) {
+        if (StringUtils.isNotBlank(fieldName)) {
+            raiseError(String.format("[Assertion failed] - argument %s is required; it must not be blank", fieldName));
+        } else {
+            raiseError("[Assertion failed] - argument is required; it must not be blank");
+        }
     }
-    return reference;
-  }
 
-  public static <T> T notNull(final T reference, final String parameterName) {
-    if (reference == null) {
-      raiseNotNullError(parameterName);
+    public static Double isGteZero(final Double value, final String parameterName) {
+        if (!ValidationUtils.isGteZero(value)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0",
+                            parameterName));
+        }
+        return value;
     }
-    return reference;
-  }
 
-  public static void isGreaterThanZero(final Long number, final String paramName) {
-    AssertUtils.notNull(number, "number");
-    if (number <= 0) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s must be greater than zero", paramName));
+    public static Integer isGteZero(final Integer value, final String parameterName) {
+        if (!ValidationUtils.isGteZero(value)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0",
+                            parameterName));
+        }
+        return value;
     }
-  }
 
-  public static void isTrue(final boolean expression, final String paramName) {
-    if (!expression) {
-      throw new IllegalArgumentException(String.format("[Assertion failed] - argument %s must be true", paramName));
+    public static Long isGteZero(final Long value, final String parameterName) {
+        if (!ValidationUtils.isGteZero(value)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0",
+                            parameterName));
+        }
+        return value;
     }
-  }
+
+    public static BigDecimal isGteZero(final BigDecimal value, final String parameterName) {
+        if (!ValidationUtils.isGteZero(value)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0",
+                            parameterName));
+        }
+        return value;
+    }
+
+    public static BigDecimal isGteOne(final BigDecimal value, final String parameterName) {
+        if (!ValidationUtils.isGte(value, BigDecimal.ONE)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 1",
+                            parameterName));
+        }
+        return value;
+    }
+
+    public static BigDecimal isGte(final BigDecimal checkValue,
+                                   final BigDecimal intervalValue,
+                                   final String parameterName) {
+        if (!ValidationUtils.isGte(checkValue, intervalValue)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater or equal than %s",
+                            parameterName, intervalValue));
+        }
+        return checkValue;
+    }
+
+    public static void isSame(final BigDecimal value1, final BigDecimal value2, final String parameterName) {
+        if (!ValidationUtils.isSame(value1, value2)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be same to %s", parameterName,
+                            value2));
+        }
+    }
+
+    public static BigDecimal isLte(final BigDecimal checkValue,
+                                   final BigDecimal intervalValue,
+                                   final String parameterName) {
+        if (!ValidationUtils.isLte(checkValue, intervalValue)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be lower or equal than %s",
+                            parameterName, intervalValue));
+        }
+        return checkValue;
+    }
+
+    public static BigDecimal isGt(final BigDecimal checkValue,
+                                  final BigDecimal intervalValue,
+                                  final String parameterName) {
+        if (!ValidationUtils.isGt(checkValue, intervalValue)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater or equal than %s",
+                            parameterName, intervalValue));
+        }
+        return checkValue;
+    }
+
+    public static BigDecimal isLt(final BigDecimal checkValue,
+                                  final BigDecimal intervalValue,
+                                  final String parameterName) {
+        if (!ValidationUtils.isLt(checkValue, intervalValue)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be lower or equal than %s",
+                            parameterName, intervalValue));
+        }
+        return checkValue;
+    }
+
+    public static BigDecimal isLteOne(final BigDecimal value, final String parameterName) {
+        if (!ValidationUtils.isLte(value, BigDecimal.ONE)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be lower or equal than 1",
+                            parameterName));
+        }
+        return value;
+    }
+
+    public static Integer isGtZero(final Integer value, final String parameterName) {
+        if (!ValidationUtils.isGtZero(value)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0",
+                            parameterName));
+        }
+        return value;
+    }
+
+    public static Long isGtZero(final Long value, final String parameterName) {
+        if (!ValidationUtils.isGtZero(value)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater or equal than 0",
+                            parameterName));
+        }
+        return value;
+    }
+
+    public static BigDecimal between(final BigDecimal value,
+                                     final BigDecimal fromInclusive,
+                                     final BigDecimal toInclusive,
+                                     final String parameterName) {
+
+        if (!ValidationUtils.between(value, fromInclusive, toInclusive)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is not within the range", parameterName));
+        }
+        return value;
+
+    }
+
+    public static BigDecimal isGtZero(final BigDecimal value, final String parameterName) {
+        if (!ValidationUtils.isGtZero(value)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater than 0",
+                            parameterName));
+        }
+        return value;
+    }
+
+    public static Double isGtZero(final Double value, final String parameterName) {
+        if (!ValidationUtils.isGtZero(value)) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s is required; it must be greater than 0",
+                            parameterName));
+        }
+        return value;
+    }
+
+    public static boolean validState(final boolean expression, final String message) {
+        if (!expression) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - the validated state is false; %s", message));
+        }
+        return expression;
+    }
+
+    public static <T> T forceNull(final T reference) {
+        if (reference != null) {
+            raiseNullError(null);
+        }
+        return reference;
+    }
+
+    public static <T> T forceNull(final T reference, final String parameterName) {
+        if (reference != null) {
+            raiseNullError(parameterName);
+        }
+        return reference;
+    }
+
+    public static <T> T notNull(final T reference) {
+        if (reference == null) {
+            raiseNotNullError(null);
+        }
+        return reference;
+    }
+
+    public static <T> T notNull(final T reference, final String parameterName) {
+        if (reference == null) {
+            raiseNotNullError(parameterName);
+        }
+        return reference;
+    }
+
+    public static Long isGreaterThanZero(final Long number, final String paramName) {
+        AssertUtils.notNull(number, "number");
+        if (number <= 0) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s must be greater than zero", paramName));
+        }
+        return number;
+    }
+
+    public static boolean isTrue(final boolean expression, final String paramName) {
+        if (!expression) {
+            throw new IllegalArgumentException(
+                    String.format("[Assertion failed] - argument %s must be true", paramName));
+        }
+        return expression;
+    }
 
 }
